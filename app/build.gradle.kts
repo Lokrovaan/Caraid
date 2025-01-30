88f161e85d0631ff9586600a88d1763dce5357be
Lokrovaan <csmith272@caledonian.ac.uk>
2025-01-30 14:29:42 +0000

    Made a form to enter account details instead of entering it into he same space as the login text fields. Still needs to be done.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.storage)
     implementation(libs.firebase.functions.ktx)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/caraid/LoginActivity.kt
+++ app/src/main/java/com/caraid/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.caraid
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -20,6 +22,9 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.caraid.ui.theme.*
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.firestore.FirebaseFirestore
+
 
 class LoginActivity : ComponentActivity() {
     private lateinit var auth: FirebaseAuth
@@ -40,6 +45,7 @@ fun LoginScreen(auth: FirebaseAuth) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var showError by remember { mutableStateOf(false) }
+    var showAccountCreationForm by remember { mutableStateOf(false) }
 
     Scaffold { paddingValues ->
         Box(
@@ -95,7 +101,12 @@ fun LoginScreen(auth: FirebaseAuth) {
                             .addOnCompleteListener { task ->
                                 if (task.isSuccessful) {
                                     // Sign in success, start MainActivity
-                                    context.startActivity(Intent(context, ChatScreenActivity::class.java))
+                                    context.startActivity(
+                                        Intent(
+                                            context,
+                                            ChatScreenActivity::class.java
+                                        )
+                                    )
                                 } else {
                                     showError = true
                                 }
@@ -110,24 +121,110 @@ fun LoginScreen(auth: FirebaseAuth) {
                 // Create Account Button
                 Button(
                     onClick = {
-                        auth.createUserWithEmailAndPassword(email, password)
-                            .addOnCompleteListener { task ->
-                                if (task.isSuccessful) {
-                                    // Account creation successful, navigate to main screen or login screen
-                                    context.startActivity(Intent(context, ChatScreenActivity::class.java))
-                                } else {
-                                    // Account creation failed, display error message
-                                    showError = true
-                                }
-                            }
-
+                        showAccountCreationForm = true
                     },
                     modifier = Modifier.padding(16.dp),
                     colors = ButtonDefaults.buttonColors(containerColor = CaraidPurpleSecondaryLight)
                 ) {
                     Text("Create Account", color = Color.White)
                 }
+                if (showAccountCreationForm) {
+                    AccountCreationForm(auth)
+                }
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AccountCreationForm(auth: FirebaseAuth) {
+    var username by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+            onClick = {
+                // Get email and password from input fields
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            // Account creation successful
+                            val userId = auth.currentUser?.uid // Get the user UID
+
+                            // Get the FCM token
+                            FirebaseMessaging.getInstance().token.addOnCompleteListener { tokenTask ->
+                                if (tokenTask.isSuccessful) {
+                                    val token = tokenTask.result
+
+                                    // Store user information in Firestore
+                                    val db = FirebaseFirestore.getInstance()
+                                    val user = hashMapOf(
+                                        "userId" to userId,
+                                        "username" to username,
+                                        "email" to email,
+                                        "fcmToken" to token
+                                    )
+                                    db.collection("users").document(userId!!)
+                                        .set(user)
+                                        .addOnSuccessListener {
+                                            Log.d("MyTag", "Account Created")
+                                            Toast.makeText(
+                                                context,
+                                                "Account created successfully!",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }
+                                        .addOnFailureListener { e ->
+                                            Log.d("MyTag", "Account Creation Failed")
+                                            Toast.makeText(
+                                                context,
+                                                "Account creation unsuccessful, please try again",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }
+                                } else {
+                                    // Handle error
+                                }
+                            }
+                        } else {
+                            // Account creation failed, display error message
+                        }
+                    }
+            },
+        ) {
+            Text("Create Account", color = Color.White)
         }
     }
 }
